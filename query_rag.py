050e5815fb95752c20b60be01484126b29d86051
azw1998 <azw1998@gmail.com>
2026-03-05 17:02:40 -0800

    get context for hard questions

--- query_rag.py
+++ query_rag.py
@@ -1,15 +1,11 @@
 from client import client
 from pathlib import Path
+from retrieve_context import retrieve_context
+import json
 
 
-def main():
-    file_path = Path("rag_docs/tables/premodel.txt")
-    schema_doc = file_path.read_text(encoding="utf-8")
-
-    question = "What was the total demand surcharge spend plus surge surcharge spend for tracking number 1Z97Y6036659856231"
-    # question = "What is the total spend for tracking number 1Z97Y6036659856231?"
-
-    response = client.responses.create(
+def get_simple_answer(client, question, schema_doc):
+    return client.responses.create(
         model="gpt-4.1-mini",
         input=[
             {
@@ -28,7 +24,23 @@ def main():
         ],
     )
 
-    print(response.output_text)
+
+def main():
+    file_path = Path("rag_docs/tables/premodel.txt")
+    schema_doc = file_path.read_text(encoding="utf-8")
+
+    question = "What surcharge id should the surge surcharge for tracking number 1Z97Y6036659856231 get?"
+    # question = "What is the total spend for tracking number 1Z97Y6036659856231?"
+
+    # answer = get_simple_answer(client, question, schema_doc)
+    # print(answer.output_text)
+    with open("embedded_docs.json", "r") as f:
+        all_chunks = json.load(f)
+    docs_ordered_by_relevance = retrieve_context(client, question, all_chunks, k=100)
+    
+    for i, doc in enumerate(docs_ordered_by_relevance):
+        print(f"Relevance score: {i+1}")
+        print(doc["document_id"], doc["entity_name"], doc["chunk_text"])
 
 
 if __name__ == "__main__":
--- retrieve_context.py
+++ retrieve_context.py
@@ -19,14 +19,12 @@ def retrieve_context(client, query, all_chunks, k=5):
 with open("embedded_docs.json", "r") as f:
     all_chunks = json.load(f)
 
-docs_ordered_by_relevance = (
-    retrieve_context(
-        client,
-        "What was the total demand surcharge spend plus surge surcharge spend for tracking number 1Z97Y6036659856231",
-        all_chunks,
-    )
+docs_ordered_by_relevance = retrieve_context(
+    client,
+    "What was the total demand surcharge spend plus surge surcharge spend for tracking number 1Z97Y6036659856231",
+    all_chunks,
 )
 
-for i,doc in enumerate(docs_ordered_by_relevance):
+for i, doc in enumerate(docs_ordered_by_relevance):
     print(f"Relevance score: {i+1}")
     print(doc["document_id"], doc["entity_name"], doc["chunk_text"])
