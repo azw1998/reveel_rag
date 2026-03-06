8cbef3d008fa179b861b6b9bc1b6f576093ff4ee
azw1998 <azw1998@gmail.com>
2026-03-06 15:03:11 -0800

    add more files

--- query_rag.py
+++ query_rag.py
@@ -11,13 +11,13 @@ def get_simple_answer(client, question, schema_doc):
             {
                 "role": "system",
                 "content": f"""
-    You generate Databricks SQL.
+    You generate Snowpark Pyspark code.
     Use the schema below.
 
     Schema:
     {schema_doc}
 
-    Return SELECT-only SQL.
+    Return a dataframe with the answer.
     """,
             },
             {"role": "user", "content": question},
@@ -26,26 +26,26 @@ def get_simple_answer(client, question, schema_doc):
 
 
 def main():
-    file_path = Path("rag_docs/tables/premodel.txt")
+    file_path = Path("rag_docs/functions/get_normalized_surcharge.txt")
     schema_doc = file_path.read_text(encoding="utf-8")
 
     question = "What surcharge id should the surge surcharge for tracking number 1Z97Y6036659856231 get?"
     # question = "What is the total spend for tracking number 1Z97Y6036659856231?"
 
-    # answer = get_simple_answer(client, question, schema_doc)
-    # print(answer.output_text)
-    with open("embedded_docs.json", "r") as f:
-        all_chunks = json.load(f)
-    docs_ordered_by_relevance = retrieve_context(client, question, all_chunks, k=100)
+    answer = get_simple_answer(client, question, schema_doc)
+    print(answer.output_text)
+    # with open("embedded_docs.json", "r") as f:
+    #     all_chunks = json.load(f)
+    # docs_ordered_by_relevance = retrieve_context(client, question, all_chunks, k=100)
 
-    test_output = "--------------------------------\n".join(
-        [
-            f'{i+1}. {doc["document_id"]}\n{doc["entity_name"]}\n{doc["document_type"]}\n{doc["chunk_text"]}'
-            for i, doc in enumerate(docs_ordered_by_relevance)
-        ]
-    )
+    # test_output = "--------------------------------\n".join(
+    #     [
+    #         f'{i+1}. {doc["document_id"]}\n{doc["entity_name"]}\n{doc["document_type"]}\n{doc["chunk_text"]}'
+    #         for i, doc in enumerate(docs_ordered_by_relevance)
+    #     ]
+    # )
 
-    print(test_output)
+    # print(test_output)
 
 
 if __name__ == "__main__":
